1f4efed8d11560a1a5c8bdf4ef33f6e09d31aff9
TyIL TTY7 <q1342893971@gmail.com>
2020-01-04 23:19:29 +0800

    [init]

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+NAME_INPUT_DIRECTORY = 'source'
+NAME_OUTPUT_DIRECTORY = 'filtered'
+
+working_dir = os.getcwd()
+
+
+def analyze_folder(
+    folder_path: str,
+    target_format: str,
+    result_list: list,
+    zip_as_folder: bool = True
+):
+    pass
+
+
+def main():
+    pass
+
+
+if (__name__ == '__main__'):
+    main()
